08a62a8b2e2a6c24600848e5a7b1e3873a821d8f
hungjack <83273308+hungjack@users.noreply.github.com>
2022-04-10 21:20:45 +0800

    更新

--- TheMaverse/js/mint.js
+++ TheMaverse/js/mint.js
@@ -1,173 +1,29 @@
-"use strict";
+$(function() {
+  $("#mint").click(function() {
+      var name = $.trim($("[name=name]").val());
+      const nameElement = document.getElementById("amount");
+      const amount = nameElement.value;
+      const price = web3.utils.toWei('0.001', 'ether')*amount;
+      var url = 'https://wtowi29z8d.execute-api.us-east-1.amazonaws.com/test/' + name;
+      $.get(url, function(data) {
+          $("pre").text(JSON.stringify(data, null, 2));
+          var proof = JSON.parse(JSON.stringify(data, null, 2));
+          //document.getElementById('message').textContent='交易處理中,請稍後';
+          console.log(proof);
+console.log(price);
+try{
+console.log("try");
+          Contract.methods.presaleMint(amount,proof).send({from:accounts[0], value:price})
+          .then(function(data){
+              console.log(data);
+      //document.getElementById('message').textContent='交易處理結束';
+          })
+}catch(e){
+  //document.getElementById('message').textContent='交易失敗';
+}
 
-/**
- * Example JavaScript code that interacts with the page and Web3 wallets
- */
+      });
 
- // Unpkg imports
-const Web3Modal = window.Web3Modal.default;
-const WalletConnectProvider = window.WalletConnectProvider.default;
-const EvmChains = window.EvmChains;
-const Fortmatic = window.Fortmatic;
-
-// Web3modal instance
-let web3Modal
-
-// Chosen wallet provider given by the dialog window
-let provider;
-
-
-// Address of the selected account
-let selectedAccount;
-
-
-/**
- * Setup the orchestra
- */
-function init() {
-
-  console.log("Initializing example");
-  console.log("WalletConnectProvider is", WalletConnectProvider);
-  console.log("Fortmatic is", Fortmatic);
-
-  // Tell Web3modal what providers we have available.
-  // Built-in web browser provider (only one can exist as a time)
-  // like MetaMask, Brave or Opera is added automatically by Web3modal
-  const providerOptions = {
-    walletconnect: {
-      package: WalletConnectProvider,
-      options: {
-        // Mikko's test key - don't copy as your mileage may vary
-        infuraId: "9aa3d95b3bc440fa88ea12eaa4456161",
-      }
-    }
-  };
-
-  web3Modal = new Web3Modal({
-    cacheProvider: false, // optional
-    providerOptions, // required
   });
 
-}
-
-
-/**
- * Kick in the UI action after Web3modal dialog has chosen a provider
- */
-async function fetchAccountData() {
-
-  // Get a Web3 instance for the wallet
-  //await ethereum.enable(); 
-  await provider.enable(); 
-  window.web3 = new Web3(provider);
-  //window.web3 = new Web3(ethereum);
-  const Contract = new web3.eth.Contract(abi,smaddress);
-
-
-  console.log("Web3 instance is", web3);
-  console.log("smart contract", Contract);
-
-  // Get connected chain id from Ethereum node
-  //const chainId = await web3.eth.getChainId();
-  // Load chain information over an HTTP API
-  //const chainData = await EvmChains.getChain(chainId);
-  //document.querySelector("#network-name").textContent = chainData.name;
-
-  // Get list of accounts of the connected wallet
-  const accounts = await web3.eth.getAccounts();
-
-  // MetaMask does not give you all accounts, only the selected account
-  console.log("Got accounts", accounts);
-  document.getElementById("name").value = accounts[0];
-  
-  // Display fully loaded UI for wallet data
-  document.querySelector("#prepare").style.display = "none";
-  document.querySelector("#connected").style.display = "block";
-}
-
-
-/**
- * Fetch account data for UI when
- * - User switches accounts in wallet
- * - User switches networks in wallet
- * - User connects wallet initially
- */
-async function refreshAccountData() {
-
-  // If any current data is displayed when
-  // the user is switching acounts in the wallet
-  // immediate hide this data
-  document.querySelector("#connected").style.display = "none";
-  document.querySelector("#prepare").style.display = "block";
-
-  // Disable button while UI is loading.
-  // fetchAccountData() will take a while as it communicates
-  // with Ethereum node via JSON-RPC and loads chain data
-  // over an API call.
-  document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
-  await fetchAccountData(provider);
-  document.querySelector("#btn-connect").removeAttribute("disabled")
-}
-
-
-/**
- * Connect wallet button pressed.
- */
-async function onConnect() {
-
-  console.log("Opening a dialog", web3Modal);
-  try {
-    provider = await web3Modal.connect();
-  } catch(e) {
-    console.log("Could not get a wallet connection", e);
-    return;
-  }
-
-  // Subscribe to accounts change
-  provider.on("accountsChanged", (accounts) => {
-    fetchAccountData();
-  });
-
-  // Subscribe to chainId change
-  provider.on("chainChanged", (chainId) => {
-    fetchAccountData();
-  });
-
-  await refreshAccountData();
-}
-
-/**
- * Disconnect wallet button pressed.
- */
-async function onDisconnect() {
-
-  console.log("Killing the wallet connection", provider);
-
-  // TODO: Which providers have close method?
-  if(provider.close) {
-    await provider.close();
-
-    // If the cached provider is not cleared,
-    // WalletConnect will default to the existing session
-    // and does not allow to re-scan the QR code with a new wallet.
-    // Depending on your use case you may want or want not his behavir.
-    await web3Modal.clearCachedProvider();
-    provider = null;
-  }
-
-  selectedAccount = null;
-
-  // Set the UI back to the initial state
-  document.querySelector("#prepare").style.display = "block";
-  document.querySelector("#connected").style.display = "none";
-}
-
-
-/**
- * Main entry point.
- */
-window.addEventListener('load', async () => {
-  document.querySelector("#btn-connect").addEventListener("click", onConnect);
 });
-
-  
